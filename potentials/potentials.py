359c0782951edf87cbd4c0fcd9f1728e48e190a1
preetham-v <preetham1104@gmail.com>
2022-10-20 11:06:09 -0700

    Added new potentials (number of contacts)

--- potentials/potentials.py
+++ potentials/potentials.py
@@ -1,7 +1,6 @@
 import torch
 
 from util import generate_Cbeta
-from icecream import ic 
 
 class Potential:
     '''
@@ -24,40 +23,6 @@ class Potential:
                                                      by taking a step along it's gradient
         '''
         raise NotImplementedError('Potential compute function was not overwritten')
-
-
-class monomer_contacts(Potential):
-    '''
-        Differentiable way to maximise number of contacts within a protein
-        
-        Motivation is given here: https://www.plumed.org/doc-v2.7/user-doc/html/_c_o_o_r_d_i_n_a_t_i_o_n.html
-
-        Author: PV
-    '''
-
-    def __init__(self, weight=1, r_0=8, d_0=2, eps=1e-6):
-
-        self.r_0       = r_0
-        self.weight    = weight
-        self.d_0       = d_0
-        self.eps       = eps
-
-    def compute(self, seq, xyz):
-
-        Ca = xyz[:,1] # [L,3]
-
-        #cdist needs a batch dimension - NRB
-        dgram = torch.cdist(Ca[None,...].contiguous(), Ca[None,...].contiguous(), p=2) # [1,Lb,Lb]
-        divide_by_r_0 = (dgram - self.d_0) / self.r_0
-        numerator = torch.pow(divide_by_r_0,6)
-        denominator = torch.pow(divide_by_r_0,12)
-
-        ncontacts = (1 - numerator) / ((1 - denominator))
-
-
-        #Potential value is the average of both radii of gyration (is avg. the best way to do this?)
-        return self.weight * ncontacts.sum()
-
 
 class monomer_ROG(Potential):
     '''
@@ -112,7 +77,166 @@ class binder_ROG(Potential):
         rad_of_gyration = torch.sqrt( torch.sum(torch.square(dgram)) / Ca.shape[0] ) # [1]
 
         return -1 * self.weight * rad_of_gyration
+
+
+class dimer_ROG(Potential):
+    '''
+        Radius of Gyration potential for encouraging compactness of both monomers when designing dimers
+
+        Author: PV
+    '''
+
+    def __init__(self, binderlen, weight=1, min_dist=15):
+
+        self.binderlen = binderlen
+        self.min_dist  = min_dist
+        self.weight    = weight
+
+    def compute(self, seq, xyz):
+
+        # Only look at monomer 1 residues
+        Ca_m1 = xyz[:self.binderlen,1] # [Lb,3]
         
+        # Only look at monomer 2 residues
+        Ca_m2 = xyz[self.binderlen:,1] # [Lb,3]
+
+        centroid_m1 = torch.mean(Ca_m1, dim=0, keepdim=True) # [1,3]
+        centroid_m2 = torch.mean(Ca_m1, dim=0, keepdim=True) # [1,3]
+
+        # cdist needs a batch dimension - NRB
+        #This calculates RoG for Monomer 1
+        dgram_m1 = torch.cdist(Ca_m1[None,...].contiguous(), centroid_m1[None,...].contiguous(), p=2) # [1,Lb,1,3]
+        dgram_m1 = torch.maximum(self.min_dist * torch.ones_like(dgram_m1.squeeze(0)), dgram_m1.squeeze(0)) # [Lb,1,3]
+        rad_of_gyration_m1 = torch.sqrt( torch.sum(torch.square(dgram_m1)) / Ca_m1.shape[0] ) # [1]
+
+        # cdist needs a batch dimension - NRB
+        #This calculates RoG for Monomer 2
+        dgram_m2 = torch.cdist(Ca_m2[None,...].contiguous(), centroid_m2[None,...].contiguous(), p=2) # [1,Lb,1,3]
+        dgram_m2 = torch.maximum(self.min_dist * torch.ones_like(dgram_m2.squeeze(0)), dgram_m2.squeeze(0)) # [Lb,1,3]
+        rad_of_gyration_m2 = torch.sqrt( torch.sum(torch.square(dgram_m2)) / Ca_m2.shape[0] ) # [1]
+
+        #Potential value is the average of both radii of gyration (is avg. the best way to do this?)
+        return -1 * self.weight * (rad_of_gyration_m1 + rad_of_gyration_m2)/2
+
+class binder_ncontacts(Potential):
+    '''
+        Differentiable way to maximise number of contacts within a protein
+        
+        Motivation is given here: https://www.plumed.org/doc-v2.7/user-doc/html/_c_o_o_r_d_i_n_a_t_i_o_n.html
+
+        Author: PV
+    '''
+
+    def __init__(self, binderlen, weight=1, r_0=8, d_0=4):
+
+        self.binderlen = binderlen
+        self.r_0       = r_0
+        self.weight    = weight
+        self.d_0       = d_0
+
+    def compute(self, seq, xyz):
+
+        # Only look at binder Ca residues
+        Ca = xyz[:self.binderlen,1] # [Lb,3]
+        
+        #cdist needs a batch dimension - NRB
+        dgram = torch.cdist(Ca[None,...].contiguous(), Ca[None,...].contiguous(), p=2) # [1,Lb,Lb]
+        divide_by_r_0 = (dgram - self.d_0) / self.r_0
+        numerator = torch.pow(divide_by_r_0,6)
+        denominator = torch.pow(divide_by_r_0,12)
+        binder_ncontacts = (1 - numerator) / (1 - denominator)
+        
+        print("BINDER CONTACTS:", binder_ncontacts.sum())
+        #Potential value is the average of both radii of gyration (is avg. the best way to do this?)
+        return self.weight * binder_ncontacts.sum()
+
+    
+class dimer_ncontacts(Potential):
+
+    '''
+        Differentiable way to maximise number of contacts for two individual monomers in a dimer
+        
+        Motivation is given here: https://www.plumed.org/doc-v2.7/user-doc/html/_c_o_o_r_d_i_n_a_t_i_o_n.html
+
+        Author: PV
+    '''
+
+
+    def __init__(self, binderlen, weight=1, r_0=8, d_0=4):
+
+        self.binderlen = binderlen
+        self.r_0       = r_0
+        self.weight    = weight
+        self.d_0       = d_0
+
+    def compute(self, seq, xyz):
+
+        # Only look at binder Ca residues
+        Ca = xyz[:self.binderlen,1] # [Lb,3]
+        #cdist needs a batch dimension - NRB
+        dgram = torch.cdist(Ca[None,...].contiguous(), Ca[None,...].contiguous(), p=2) # [1,Lb,Lb]
+        divide_by_r_0 = (dgram - self.d_0) / self.r_0
+        numerator = torch.pow(divide_by_r_0,6)
+        denominator = torch.pow(divide_by_r_0,12)
+        binder_ncontacts = (1 - numerator) / (1 - denominator)
+        #Potential is the sum of values in the tensor
+        binder_ncontacts = binder_ncontacts.sum()
+
+        # Only look at target Ca residues
+        Ca = xyz[self.binderlen:,1] # [Lb,3]
+        dgram = torch.cdist(Ca[None,...].contiguous(), Ca[None,...].contiguous(), p=2) # [1,Lb,Lb]
+        divide_by_r_0 = (dgram - self.d_0) / self.r_0
+        numerator = torch.pow(divide_by_r_0,6)
+        denominator = torch.pow(divide_by_r_0,12)
+        target_ncontacts = (1 - numerator) / (1 - denominator)
+        #Potential is the sum of values in the tensor
+        target_ncontacts = target_ncontacts.sum()
+        
+        print("DIMER NCONTACTS:", (binder_ncontacts+target_ncontacts)/2)
+        #Returns average of n contacts withiin monomer 1 and monomer 2
+        return self.weight * (binder_ncontacts+target_ncontacts)/2
+
+class interface_ncontacts(Potential):
+
+    '''
+        Differentiable way to maximise number of contacts between binder and target
+        
+        Motivation is given here: https://www.plumed.org/doc-v2.7/user-doc/html/_c_o_o_r_d_i_n_a_t_i_o_n.html
+
+        Author: PV
+    '''
+
+
+    def __init__(self, binderlen, weight=1, r_0=8, d_0=6):
+
+        self.binderlen = binderlen
+        self.r_0       = r_0
+        self.weight    = weight
+        self.d_0       = d_0
+
+    def compute(self, seq, xyz):
+
+        # Extract binder Ca residues
+        Ca_b = xyz[:self.binderlen,1] # [Lb,3]
+
+        # Extract target Ca residues
+        Ca_t = xyz[self.binderlen:,1] # [Lt,3]
+
+        #cdist needs a batch dimension - NRB
+        dgram = torch.cdist(Ca_b[None,...].contiguous(), Ca_t[None,...].contiguous(), p=2) # [1,Lb,Lt]
+        divide_by_r_0 = (dgram - self.d_0) / self.r_0
+        numerator = torch.pow(divide_by_r_0,6)
+        denominator = torch.pow(divide_by_r_0,12)
+        interface_ncontacts = (1 - numerator) / (1 - denominator)
+        #Potential is the sum of values in the tensor
+        interface_ncontacts = interface_ncontacts.sum()
+
+        print("INTERFACE CONTACTS:", interface_ncontacts.sum())
+
+        return self.weight * interface_ncontacts
+
+
+
 class binder_distance_ReLU(Potential):
     '''
         Given the current coordinates of the diffusion trajectory, calculate a potential that is the distance between each residue
@@ -232,11 +356,18 @@ implemented_potentials = { 'monomer_ROG':          monomer_ROG,
                            'binder_ROG':           binder_ROG,
                            'binder_distance_ReLU': binder_distance_ReLU,
                            'binder_any_ReLU':      binder_any_ReLU,
-                           'monomer_contacts':     monomer_contacts}
+                           'dimer_ROG':            dimer_ROG,
+                           'binder_ncontacts':     binder_ncontacts,
+                           'dimer_ncontacts':      dimer_ncontacts,
+                           'interface_ncontacts':  interface_ncontacts}
 
 require_binderlen      = { 'binder_ROG',
                            'binder_distance_ReLU',
-                           'binder_any_ReLU' }
+                           'binder_any_ReLU',
+                           'dimer_ROG',
+                           'binder_ncontacts',
+                           'dimer_ncontacts',
+                           'interface_ncontacts'}
 
 require_hotspot_res    = { 'binder_distance_ReLU',
                            'binder_any_ReLU' }
