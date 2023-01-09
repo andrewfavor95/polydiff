Fix MSA masking

apply_masks.py
@@ -8,7 +8,8 @@ from diffusion import get_beta_schedule
 from inference.utils import get_next_ca, get_next_frames
 import rf2aa.tensor_util
 import rf2aa.chemical
-from rf2aa.chemical import NAATOKENS
+from rf2aa.chemical import NAATOKENS, MASKINDEX
+from rf2aa.tensor_util import assert_equal
 
 ic.configureOutput(includeContext=True)
 
@@ -382,11 +383,11 @@ def mask_inputs(seq,
         # raise Exception('needs to be adapted to aa')
         assert len(blosum_replacement[0]) == decoded_non_motif[0].sum() and len(blosum_replacement[1]) == decoded_non_motif[1].sum()
         seq = torch.nn.functional.one_hot(seq, num_classes=rf2aa.chemical.NAATOKENS).float() # [n,I,L,22]
-        # seq[0,:,~seq_mask[0],:N_TOKEN-1] = 0
-        seq[1,:,~seq_mask[1],:21] = 0 
+        seq[0,:,~seq_mask[0],:] = 0
+        seq[1,:,~seq_mask[1],:] = 0 
 
-        seq[0,:,~seq_mask[0],21] = 1 # mask token categorical value
-        seq[1,:,~seq_mask[1],21] = 1 # mask token categorical value
+        seq[0,:,~seq_mask[0],MASKINDEX] = 1 # mask token categorical value
+        seq[1,:,~seq_mask[1],MASKINDEX] = 1 # mask token categorical value
 
         seq[0,:,decoded_non_motif[0],:22] = onehot_blosum_rep[0]
         seq[1,:,decoded_non_motif[1],:22] = onehot_blosum_rep[1] 
@@ -411,8 +412,8 @@ def mask_inputs(seq,
         msa_masked[0,:,:,~seq_mask[0],:NAATOKENS-1] = 0
         msa_masked[1,:,:,~seq_mask[0],:NAATOKENS-1] = 0
 
-        msa_masked[0,:,:,~seq_mask[0],NAATOKENS-1]  = 1 # set to mask token
-        msa_masked[1,:,:,~seq_mask[1],NAATOKENS-1]  = 1 # set to mask token
+        msa_masked[0,:,:,~seq_mask[0],MASKINDEX]  = 1 # set to mask token
+        msa_masked[1,:,:,~seq_mask[1],MASKINDEX]  = 1 # set to mask token
 
         # index 44/45 is insertion/deletion
         # index 43 is the masked token NOTE check this
@@ -420,8 +421,8 @@ def mask_inputs(seq,
         msa_masked[0,:,:,~seq_mask[0],NAATOKENS:2*NAATOKENS-1] = 0
         msa_masked[1,:,:,~seq_mask[1],NAATOKENS:2*NAATOKENS-1] = 0
 
-        msa_masked[0,:,:,~seq_mask[0],2*NAATOKENS-1]    = 1
-        msa_masked[1,:,:,~seq_mask[1],2*NAATOKENS-1]    = 1 
+        msa_masked[0,:,:,~seq_mask[0],NAATOKENS+MASKINDEX]    = 1
+        msa_masked[1,:,:,~seq_mask[1],NAATOKENS+MASKINDEX]    = 1 
 
         # insertion/deletion stuff 
         msa_masked[0,:,:,~seq_mask[0],2*NAATOKENS:2*NAATOKENS+2*NINDEL] = 0
@@ -458,11 +459,11 @@ def mask_inputs(seq,
         msa_full[0,:,:,~seq_mask[0],:NAATOKENS-1] = 0
         msa_full[1,:,:,~seq_mask[1],:NAATOKENS-1] = 0
 
-        msa_full[0,:,:,~seq_mask[0],NAATOKENS-1]  = 1
-        msa_full[1,:,:,~seq_mask[1],NAATOKENS-1]  = 1
+        msa_full[0,:,:,~seq_mask[0],MASKINDEX]  = 1
+        msa_full[1,:,:,~seq_mask[1],MASKINDEX]  = 1
 
-        msa_full[0,:,:,~seq_mask[0],-3:] = 0   
-        msa_full[1,:,:,~seq_mask[1],-3:] = 0   #NOTE: double check this is insertions/deletions and 0 makes sense 
+        msa_full[0,:,:,~seq_mask[0],NAATOKENS:NAATOKENS+NINDEL] = 0   
+        msa_full[1,:,:,~seq_mask[1],NAATOKENS:NAATOKENS+NINDEL] = 0   #NOTE: double check this is insertions/deletions and 0 makes sense 
 
         # blosum mutations 
         msa_full[0,:,:,decoded_non_motif[0],:] = 0
@@ -488,19 +489,40 @@ def mask_inputs(seq,
         t1d[:,:,:,22] = input_t1d_seq_conf_mask[:,None,:]
     else:
         # TODO: adapt this for small molecules using t1d shift
+        # Mask the diffused sequence
+
+        # # ic(blosum_replacement.shape)
+        # ic(blosum_replacement)
+        # blosum_replacement = torch.cat(blosum_replacement)
+        # ic(blosum_replacement.shape)
+        # seq_cat_shifted = blosum_replacement.argmax(dim=-1)
+        # ic(seq_cat_shifted)
+        # seq_cat_shifted[seq_cat_shifted>=MASKINDEX] -= 1
+        # # t1d_motif = torch.nn.functional.one_hot(seq_cat_shifted, num_classes=NAATOKENS-1)
+        # # ic(t1d)
+        # # t1d = t1d[None, None] # [L, NAATOKENS-1] --> [1,1,L, NAATOKENS-1]
+
+        # ic(t1d.shape, t1d.argmax(dim=-1))
+
         t1d[0,:,~seq_mask[0],:20] = 0 
         t1d[1,:,~seq_mask[1],:20] = 0 
 
         t1d[0,:,~seq_mask[0],20]  = 1
         t1d[1,:,~seq_mask[1],20]  = 1 # unknown
 
+        # ONLY FOR aa_decoding
+        # Add the motif sequence
+        t1d_before = torch.clone(t1d)
         t1d[0,:,decoded_non_motif[0],:]  = 0
         t1d[1,:,decoded_non_motif[1],:]  = 0
 
         t1d[0,:,decoded_non_motif[0],blosum_replacement[0]] = 1
         t1d[1,:,decoded_non_motif[1],blosum_replacement[1]] = 1
+        # Sanity check that t1d is not altered when not doing aa decoding.
+        if diffusion_param['aa_decode_steps'] == 0:
+            rf2aa.tensor_util.assert_equal(t1d, t1d_before)
 
-        t1d[:,:,:,21] = input_t1d_str_conf_mask[:,None,:]
+        t1d[:,:,:,-1] = input_t1d_str_conf_mask[:,None,:]
     
     # mask sidechains in the diffused region if preprocess_param['sidechain_input'] is False
     if preprocess_param['sidechain_input'] is False:

mask_generator.py
@@ -144,7 +144,7 @@ def get_sm_contacts(xyz, is_atom, is_sm,
     dist_conf = (picked[:, None] - sm_crds[ None]).pow(2).sum(dim=-1).sqrt()
     dist_conf = dist_conf.nan_to_num(9999)
     picked_distances = dist_conf.min(-1)[0].min(-1)[0]
-    ic(is_motif, n_sample, picked_distances, dist_cutoff, indices)
+    #ic(is_motif, n_sample, picked_distances, dist_cutoff, indices)
 
     return is_motif
 
@@ -244,7 +244,7 @@ def generate_sm_mask(prot_masks, is_sm):
             continue
         if k == 'loss_str_mask_2d':
             continue
-        ic(k, v.shape, prot_masks[k].shape, is_sm.shape)
+        #ic(k, v.shape, prot_masks[k].shape, is_sm.shape)
         v[~is_sm] = prot_masks[k]
         mask_dict[k] = v
     mask_dict['input_seq_mask']

run_inference.py
@@ -35,7 +35,7 @@ import numpy as np
 import random
 import glob
 import inference.model_runners
-ic.configureOutput(includeContext=True)
+# ic.configureOutput(includeContext=True)
 
 def make_deterministic(seed=0):
         torch.manual_seed(seed)

train_multi_deep.py
@@ -27,6 +27,7 @@ import rf2aa.data_loader
 import rf2aa.util
 import rf2aa.loss
 import rf2aa.tensor_util
+from rf2aa.tensor_util import assert_equal
 from rf2aa.util_module import ComputeAllAtomCoords
 from rf2aa.RoseTTAFoldModel import RoseTTAFoldModule
 from data_loader import (
@@ -760,6 +761,7 @@ class Trainer():
             all_param.update(self.loss_param)
             all_param.update(self.diffusion_param)
 
+            # wandb.config.update(all_param)
             wandb.config = all_param
             wandb.save(os.path.join(os.getcwd(), self.outdir, 'git_diff.txt'))
         ic(os.environ['MASTER_ADDR'], rank, world_size, torch.cuda.device_count())
@@ -1180,13 +1182,13 @@ class Trainer():
 
             # Since we are not sequence self-conditioning, much of 2-terminal index is redundant.
             # Let's assert that it's the same.
-            # # Not true due to blosum
-            # rf2aa.tensor_util.assert_equal(seq[:,0],seq[:,1])
-            # # rf2aa.tensor_util.assert_equal(t1d[:,0],t1d[:,1]) # Last idx unequal due to confidence
-            # # rf2aa.tensor_util.assert_equal(alpha_t[:,0],alpha_t[:,1]) # Unequal due to coming from different xyz_t
-            # rf2aa.tensor_util.assert_equal(msa_masked[:,0],msa_masked[:,1])
-            # rf2aa.tensor_util.assert_equal(msa_full[:,0],msa_full[:,1])
-            # rf2aa.tensor_util.assert_equal(mask_msa[:,0],mask_msa[:,1])
+            # Not true due to blosum
+            rf2aa.tensor_util.assert_equal(seq[:,0],seq[:,1])
+            # rf2aa.tensor_util.assert_equal(t1d[:,0],t1d[:,1]) # Last idx unequal due to confidence
+            # rf2aa.tensor_util.assert_equal(alpha_t[:,0],alpha_t[:,1]) # Unequal due to coming from different xyz_t
+            rf2aa.tensor_util.assert_equal(msa_masked[:,0],msa_masked[:,1])
+            rf2aa.tensor_util.assert_equal(msa_full[:,0],msa_full[:,1])
+            rf2aa.tensor_util.assert_equal(mask_msa[:,0],mask_msa[:,1])
 
             # From here on out we will operate with t = t
             seq        = seq[:,1]
@@ -1219,7 +1221,7 @@ class Trainer():
                     xyz_t = torch.cat((px0_xyz.unsqueeze(1),zeros), dim=-2) # [B,T,L,27,3]
                     t2d, mask_t_2d_remade = util.get_t2d(
                         # xyz_t[0], mask_t[0], seq_scalar[0], same_chain[0], atom_frames[0])
-                        xyz_t[0], is_sm, atom_frames)
+                        xyz_t[0], is_sm[0], atom_frames[0])
                     t2d = t2d[None] # Add batch dimension # [B,T,L,L,44]
                 else:
                     xyz_t = torch.zeros_like(xyz_t[:,1])
@@ -1455,6 +1457,7 @@ class Trainer():
                 m, s = divmod(expected_epoch_time, 60)
                 h, m = divmod(m, 60)
                 n_rates = len(self.rate_deque)
+                ic(mean_rate, self.n_epoch, expected_epoch_time, self.batch_size, world_size)
                 print(f'Expected time per epoch (h:m:s) based off {n_rates} measured pseudo batch times: {h:d}:{m:02d}:{s:.0f}')
 
             #     res_mask = 
@@ -1513,6 +1516,7 @@ def make_trainer(args, model_param, loader_param, loss_param, diffusion_param, p
         # loader_param['DATAPKL'] = 'subsampled_dataset.pkl'
         # loader_param['DATAPKL_AA'] = 'subsampled_all-atom-dataset.pkl'
         os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+        ic.configureOutput(includeContext=True)
     else:
         DEBUG = False 
         WANDB = True 