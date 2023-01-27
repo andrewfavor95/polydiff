68429a80cb1bdbaba7bc9d403677bfa1dbfa9671
Woody Ahern <ahern@uw.edu>
2023-01-26 21:26:04 -0800

    Add training regression test.

---
+++ test_training_regression.py
@@ -0,0 +1,55 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'RF2-allatom'))
+from rf2aa.RoseTTAFoldModel import RoseTTAFoldModule
+import torch
+import shlex
+import unittest
+import subprocess
+from pathlib import Path
+from unittest import mock
+from icecream import ic
+import run_inference
+from deepdiff import DeepDiff
+from rf2aa import tensor_util
+
+from inspect import signature
+
+import test_utils
+import train_multi_deep
+
+REWRITE = False
+class CallException(Exception):
+    pass
+
+class TestRegression(unittest.TestCase):
+    
+    # Example regression test.
+    def test_regression(self):
+        run_inference.make_deterministic()
+        # Uncomment to assert test is checking the appropriate things.
+        # run_inference.make_deterministic(1)
+        path = 'goldens'
+        from arguments import get_args
+        argString = "-p_drop 0.15 -accum 2 -crop 100 -w_disp 0.5 -w_frame_dist 1.0 -w_aa 0 -w_blen 0.0 -w_bang 0.0 -w_lj 0.0 -w_hb 0.0 -w_str 0.0 -maxlat 256 -maxseq 1024 -num_epochs 2 -lr 0.0005 -seed 42 -seqid 150.0 -mintplt 1 -use_H -max_length 100 -max_complex_chain 250 -task_names diff,seq2str -task_p 1.0,0.0 -diff_T 200 -aa_decode_steps 0 -wandb_prefix debug_sm_conditional -diff_so3_type igso3 -diff_chi_type interp -use_tschedule -maxcycle 1 -diff_b0 0.01 -diff_bT 0.07 -diff_schedule_type linear -prob_self_cond 0.5 -str_self_cond -dataset pdb_aa,sm_complex -dataset_prob 0.0,1.0 -sidechain_input False -motif_sidechain_input True -ckpt_load_path /home/ahern/projects/rf_diffusion/train_session2023-01-09_1673291857.7027779/models/BFF_4.pt -d_t1d 22 -new_self_cond -diff_crd_scale 0.25 -metric displacement -metric contigs -diff_mask_probs get_triple_contact:1.0 -w_motif_disp 10     -data_pkl test_dataset_100.pkl -data_pkl_aa test_allatom_dataset_100.pkl     -n_extra_block 4     -n_main_block 32     -n_ref_block 4     -n_finetune_block 0     -ref_num_layers 2     -d_pair 192     -n_head_pair 6     -freeze_track_motif     -interactive     -n_write_pdb 1 -zero_weights     -debug"
+        split_args = shlex.split(argString)
+        all_args = get_args(split_args)
+
+        func_sig = signature(RoseTTAFoldModule.forward)
+        with mock.patch.object(RoseTTAFoldModule, "forward") as submethod_mocked:
+            submethod_mocked.side_effect = CallException('Function called!')
+            train = train_multi_deep.make_trainer(*all_args)
+            try:
+                train.run_model_training(torch.cuda.device_count())
+            except CallException:
+                print("Called!")
+
+            args, kwargs = submethod_mocked.call_args
+            args = (None,) + args
+            argument_binding = func_sig.bind(*args, **kwargs)
+            argument_map = argument_binding.arguments
+            argument_map = tensor_util.cpu(argument_map)
+            test_utils.assert_matches_golden(self, 'model_input_0', argument_map, rewrite=REWRITE, custom_comparator=tensor_util.cmp)
+
+if __name__ == '__main__':
+        unittest.main()
---
+++ test_utils.py
@@ -0,0 +1,170 @@
+import re
+import os
+import subprocess
+from pathlib import Path
+import yaml
+import unittest
+import pickle
+
+golden_dir = 'goldens'
+
+
+def golden_path(name, processor_specific=False):
+    processor_name = 'any'
+    if processor_specific:
+        processor_name = processor()
+    return Path(os.path.join(golden_dir, f'{name}_{processor_name}'))
+
+def get_golden_path(name):
+    p = golden_path()
+    if not p.exists():
+        raise Exception(f'golden at {p} does not exist, please generate it')
+    return p
+
+
+def write(path, got):
+    with open(path, 'wb') as fh:
+        pickle.dump(got, fh)
+
+def read(path):
+    with open(path, 'rb') as fh:
+        return pickle.load(fh)
+
+def assert_matches_golden(t, name, got, rewrite=False, processor_specific=False, custom_comparator=None):
+    p = golden_path(name, processor_specific=processor_specific)
+    if rewrite:
+        print(f'rewriting {p}')
+        write(p, got)
+        # p.write_text(got)
+        return
+    if not p.exists():
+        raise Exception(f'golden at {p} does not exist, please generate it')
+    # want = p.read_text()
+    want = read(p)
+    if custom_comparator:
+        diff = custom_comparator(got, want)
+        if diff:
+            t.fail(diff)
+    else:
+        t.assertEqual(got, want)
+
+
+def processor():
+    o = subprocess.run(['lscpu | grep "Model name"'], capture_output=True, shell=True)
+    s = o.stdout.decode("utf-8")
+    s = s[len("Model name:"):].strip()
+    ncpus = available_cpu_count()
+    s = f'{s}-{ncpus}CPUs'
+    return s
+
+def available_cpu_count():
+    """ Number of available virtual or physical CPUs on this system, i.e.
+    user/real as output by time(1) when called with an optimally scaling
+    userspace-only program"""
+
+    # cpuset
+    # cpuset may restrict the number of *available* processors
+    try:
+        m = re.search(r'(?m)^Cpus_allowed:\s*(.*)$',
+                      open('/proc/self/status').read())
+        if m:
+            res = bin(int(m.group(1).replace(',', ''), 16)).count('1')
+            if res > 0:
+                return res
+    except IOError:
+        pass
+
+    # Python 2.6+
+    try:
+        import multiprocessing
+        return multiprocessing.cpu_count()
+    except (ImportError, NotImplementedError):
+        pass
+
+    # https://github.com/giampaolo/psutil
+    try:
+        import psutil
+        return psutil.cpu_count()   # psutil.NUM_CPUS on old versions
+    except (ImportError, AttributeError):
+        pass
+
+    # POSIX
+    try:
+        res = int(os.sysconf('SC_NPROCESSORS_ONLN'))
+
+        if res > 0:
+            return res
+    except (AttributeError, ValueError):
+        pass
+
+    # Windows
+    try:
+        res = int(os.environ['NUMBER_OF_PROCESSORS'])
+
+        if res > 0:
+            return res
+    except (KeyError, ValueError):
+        pass
+
+    # jython
+    try:
+        from java.lang import Runtime
+        runtime = Runtime.getRuntime()
+        res = runtime.availableProcessors()
+        if res > 0:
+            return res
+    except ImportError:
+        pass
+
+    # BSD
+    try:
+        sysctl = subprocess.Popen(['sysctl', '-n', 'hw.ncpu'],
+                                  stdout=subprocess.PIPE)
+        scStdout = sysctl.communicate()[0]
+        res = int(scStdout)
+
+        if res > 0:
+            return res
+    except (OSError, ValueError):
+        pass
+
+    # Linux
+    try:
+        res = open('/proc/cpuinfo').read().count('processor\t:')
+
+        if res > 0:
+            return res
+    except IOError:
+        pass
+
+    # Solaris
+    try:
+        pseudoDevices = os.listdir('/devices/pseudo/')
+        res = 0
+        for pd in pseudoDevices:
+            if re.match(r'^cpuid@[0-9]+$', pd):
+                res += 1
+
+        if res > 0:
+            return res
+    except OSError:
+        pass
+
+    # Other UNIXes (heuristic)
+    try:
+        try:
+            dmesg = open('/var/run/dmesg.boot').read()
+        except IOError:
+            dmesgProcess = subprocess.Popen(['dmesg'], stdout=subprocess.PIPE)
+            dmesg = dmesgProcess.communicate()[0]
+
+        res = 0
+        while '\ncpu' + str(res) + ':' in dmesg:
+            res += 1
+
+        if res > 0:
+            return res
+    except OSError:
+        pass
+
+    raise Exception('Can not determine number of CPUs on this system')
