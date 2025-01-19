84cfe8729a926d1f7647502f376c037b94466f70
afavor <afavor@uw.edu>
2025-01-19 13:19:01 -0800

    updates to parser function

--- inference/utils.py
+++ inference/utils.py
@@ -1391,139 +1391,34 @@ def parse_pdb(filename, **kwargs):
     '''extract xyz coords for all heavy atoms'''
     lines = open(filename,'r').readlines()
     return parse_pdb_lines(lines, **kwargs)
-    # return parse_pdb_na(lines, **kwargs)
 
-# def parse_pdb(filename, **kwargs):
-#     '''extract xyz coords for all heavy atoms'''
-#     lines = open(filename,'r').readlines()
-#     return parse_pdb_lines(lines, **kwargs)
-
-# def parse_pdb_na(lines, parse_hetatom=False, ignore_het_h=True):
-#     """
-#     adapted from code sent by Ryan
-#     """
-#     res = []
-#     for l in lines:
-#         split = l.split()
-#         if len(split) < 5:
-#             continue
-#         if split[0] == 'ATOM' and split[2] == "CA":
-#             res.append((split[5], split[3], split[4]))
-#         elif split[0] == 'ATOM' and split[2] == "C1'":
-#             res.append((split[5], ' ' + split[3], split[4]))
-
-
-#     # res = [(l[22:26],l[17:20],l[21]) for l in lines if \
-#     #         l[:4]=="ATOM" and l[12:16].strip() in ["CA","C1'"] and l[16] in [" ","A"]]
-#     # print('res',res)
-#     # idx_s = [(int(r[0]),r[2]) for r in res]
-#     idx_s = [(r[2], int(r[0])) for r in res]
-#     seq = [rf2aa.chemical.aa2num[r[1]] if r[1] in rf2aa.chemical.aa2num.keys() else 20 for r in res]
-#     chains = []
-#     for r in res:
-#         if r[2] not in chains:
-#             chains.append(r[2])
-#     L_s = [sum([1 for r in res if r[2] == x]) for x in chains]
-#     #print('L_s',L_s)
-#     xyz = np.full((len(idx_s), rf2aa.chemical.NTOTAL, 3), np.nan, dtype=np.float32)
-#     for l in lines:
-#         if len(l) < 5 or l[:4] != "ATOM":
-#             continue
-#         split = l.split()
-#         resNo, atom, aa, chid = int(split[5]), split[2], split[3], split[4]
-#         if aa in ['DA', 'DC', 'DG', 'DT', 'DX']:
-#             aa = ' ' + aa
-#         # resNo, atom, aa, chid = int(l[22:26]), l[12:16], l[17:20], l[21]
-#         #print(resNo, atom, aa)
-#         if (chid,resNo) not in idx_s:
-#             continue
-#         idx = idx_s.index((chid,resNo))
-#         if aa not in rf2aa.chemical.aa2num.keys():
-#             continue
-#         for i_atm, tgtatm in enumerate(rf2aa.chemical.aa2long[rf2aa.chemical.aa2num[aa]]):
-#             if tgtatm and tgtatm.strip() == atom:
-#                 #print(i_atm, tgtatm)
-#                 # xyz[idx,i_atm,:] = [float(l[30:38]), float(l[38:46]), float(l[46:54])]
-#                 xyz[idx,i_atm,:] = [float(split[6]), float(split[7]), float(split[8])]
-#                 break
+def parse_pdb_lines(lines, parse_hetatom=False, ignore_het_h=True,
+                    seq_atom_order=["CA", "C1'", "P", "O5'", "C5'", "O4'"], # for checking seq
+                    aa_remapping = {'  A': ' RA', '  U': ' RU', '  G': ' RG', '  C': ' RC'},
+    ):
     
-#     mask = np.logical_not(np.isnan(xyz[...,0]))
-#     seq = np.array(seq)
-#     #xyz[np.isnan(xyz[...,0])] = 0.0
-#     # pdbs = []
-    
-#     # HACK should find a better place for this
-#     residue_mask = ~np.isnan(xyz)[:, :3].any(axis=(1, 2))
-#     xyz = xyz[residue_mask]
-#     # idx = idx[residue_mask]
-#     seq = seq[residue_mask]
-#     mask = mask[residue_mask]
-#     idx_s = [idxpdb for idxpdb, resmask in zip(idx_s, residue_mask) if resmask]
-#     out = {
-#         'xyz': xyz,
-#         'mask': mask,
-#         'idx': np.array([i[1] for i in idx_s]),
-#         'seq': seq,
-#         'pdb_idx': idx_s
-#     }
-
-#     return out
-
-def parse_pdb_lines(lines, parse_hetatom=False, ignore_het_h=True):
-    # indices of residues observed in the structure
-    
-
     res = []
     pdb_idx = []
-    prev_idx_string = 'XXXX'
+    prev_resNo = 'XXXX'
     for l in lines:
-        split = l.split()
-        if len(split) < 5:
+        if len(l) < 5 or l[:4] != "ATOM":
             continue
 
-        # if l[:4]=="ATOM":
-        #     atm_string = l[12:16]
-        #     res_string = l[17:20]
-        #     chn_string = l[21:22]
-        #     idx_string = l[22:26]
+        chain, resNo, atom, aa, lddt = l[21:22].strip(), int(l[22:26]), l[12:16], l[17:20], l[60:66].strip()
+        # Replace some standard pdb 3-letter codes with RF-convention tokens:
+        aa = aa_remapping.get(aa, aa)
 
-        if split[0]=="ATOM":
-            atm_string, res_string, chn_string, idx_string = split[2:6]
-
-            # Only add this stuff if we haven't added anything for this resi yet:
-            if not idx_string==prev_idx_string:
-
-                # Break this into tower of conditionals so we can exit once we get a valid atom:
-                # have to do it this way for a ranking in atom priority, sadly.
-                # update this prev_idx_string so we can compare to it in the next iter
-                if atm_string.strip()=="CA":
-                    res.append((idx_string, res_string))
-                    pdb_idx.append(( chn_string.strip(), int(idx_string.strip()) ))
-                    prev_idx_string = idx_string
-
-                elif atm_string.strip()=="P":
-                    res.append((idx_string, ' ' +res_string))
-                    pdb_idx.append(( chn_string.strip(), int(idx_string.strip()) ))
-                    prev_idx_string = idx_string
-
-                elif atm_string.strip()=="O5'":
-                    res.append((idx_string, ' ' +res_string))
-                    pdb_idx.append(( chn_string.strip(), int(idx_string.strip()) ))
-                    prev_idx_string = idx_string
-
-                elif atm_string.strip()=="C1'":
-                    res.append((idx_string, ' ' +res_string))
-                    pdb_idx.append(( chn_string.strip(), int(idx_string.strip()) ))
-                    prev_idx_string = idx_string
-
-                # elif atm_string.strip()=="C5'":
-                #     print("Really not an ideal situation if we have to use C5 prime...")
-                #     res.append((idx_string, res_string))
-                #     pdb_idx.append(( chn_string.strip(), int(idx_string.strip()) ))
-                #     prev_idx_string = idx_string
+        # Only add this residue if we haven't already added it
+        if not resNo==prev_resNo: 
+            for seq_atom in seq_atom_order:
+                if atom.strip() == seq_atom:
+                    res.append((chain, resNo, aa, lddt))
+                    pdb_idx.append(( chain, resNo ))
+                    prev_resNo = resNo  # Update to avoid duplicate residues
+                    break  # Stop checking once we find the first matching atom
 
     idx_s = [(i[0], i[1]) for i in pdb_idx]
-    seq = [util.aa2num[r[1]] if r[1] in rf2aa.chemical.aa2num.keys() else 20 for r in res]
+    seq = [util.aa2num[r[2]] if r[2] in rf2aa.chemical.aa2num.keys() else 20 for r in res]
     chains = []
     
     for c in pdb_idx:
@@ -1536,29 +1431,22 @@ def parse_pdb_lines(lines, parse_hetatom=False, ignore_het_h=True):
     for l in lines:
         if len(l) < 5 or l[:4] != "ATOM":
             continue
+
         split = l.split()
-        resNo, atom, aa, chid = int(split[5]), split[2], split[3], split[4]
-        # If it is a nucleic acid using the RoseTTAfold naming conventions:
-        if aa in ['DA', 'DC', 'DG', 'DT', 'DX', 'RA', 'RC', 'RG', 'RU', 'RX',]:
-            aa = ' ' + aa
-        # If it is a nucleic acid using the RoseTTAfold naming conventions:
-        elif aa in ['A','C','G','U']:
-            aa = ' R' + aa
+        chain, resNo, atom, aa = l[21:22].strip(), int(l[22:26]), l[12:16], l[17:20]
+        # Replace some standard pdb 3-letter codes with RF-convention tokens:
+        aa = aa_remapping.get(aa, aa)
 
-
-        # resNo, atom, aa, chid = int(l[22:26]), l[12:16], l[17:20], l[21]
-        #print(resNo, atom, aa)
-        if (chid,resNo) not in idx_s:
+        if (chain,resNo) not in idx_s:
             continue
-        idx = idx_s.index((chid,resNo))
+        idx = idx_s.index((chain,resNo))
         if aa not in rf2aa.chemical.aa2num.keys():
             continue
-
+            
         for i_atm, tgtatm in enumerate(rf2aa.chemical.aa2long[rf2aa.chemical.aa2num[aa]]):
-            if tgtatm and tgtatm.strip() == atom:
-                #print(i_atm, tgtatm)
-                # xyz[idx,i_atm,:] = [float(l[30:38]), float(l[38:46]), float(l[46:54])]
-                xyz[idx,i_atm,:] = [float(split[6]), float(split[7]), float(split[8])]
+            # if tgtatm and tgtatm == atom:
+            if tgtatm == atom:
+                xyz[idx,i_atm,:] = [float(l[30:38]), float(l[38:46]), float(l[46:54])]
                 break
 
     # save atom mask
@@ -1587,6 +1475,7 @@ def parse_pdb_lines(lines, parse_hetatom=False, ignore_het_h=True):
     mask = mask[residue_mask]
     idx_s = [idxpdb for idxpdb, resmask in zip(idx_s, residue_mask) if resmask]
 
+
     out = {
         'xyz':xyz, # cartesian coordinates, [Lx14]
         'mask':mask, # mask showing which atoms are present in the PDB file, [Lx14]
@@ -1613,242 +1502,6 @@ def parse_pdb_lines(lines, parse_hetatom=False, ignore_het_h=True):
         out['info_het'] = info_het
         
     return out
-
-
-# def parse_pdb_lines(lines, parse_hetatom=False, ignore_het_h=True):
-#     # indices of residues observed in the structure
-    
-
-#     res = []
-#     pdb_idx = []
-#     prev_idx_string = 'XXXX'
-#     for l in lines:
-#         if l[:4]=="ATOM":
-#             atm_string = l[12:16]
-#             res_string = l[17:20]
-#             chn_string = l[21:22]
-#             idx_string = l[22:26]
-
-#             # Only add this stuff if we haven't added anything for this resi yet:
-#             if not idx_string==prev_idx_string:
-
-#                 # Break this into tower of conditionals so we can exit once we get a valid atom:
-#                 # have to do it this way for a ranking in atom priority, sadly.
-#                 # update this prev_idx_string so we can compare to it in the next iter
-#                 if atm_string.strip()=="CA":
-#                     res.append((idx_string, res_string))
-#                     pdb_idx.append(( chn_string.strip(), int(idx_string.strip()) ))
-#                     prev_idx_string = idx_string
-
-#                 elif atm_string.strip()=="P":
-#                     res.append((idx_string, res_string))
-#                     pdb_idx.append(( chn_string.strip(), int(idx_string.strip()) ))
-#                     prev_idx_string = idx_string
-
-#                 elif atm_string.strip()=="O5'":
-#                     res.append((idx_string, res_string))
-#                     pdb_idx.append(( chn_string.strip(), int(idx_string.strip()) ))
-#                     prev_idx_string = idx_string
-
-#                 elif atm_string.strip()=="C5'":
-#                     print("Really not an ideal situation if we have to use C5 prime...")
-#                     res.append((idx_string, res_string))
-#                     pdb_idx.append(( chn_string.strip(), int(idx_string.strip()) ))
-#                     prev_idx_string = idx_string
-
-#     seq = [util.aa2num[r[1]] if r[1] in util.aa2num.keys() else 20 for r in res]
-
-#     # 4 BB + up to 10 SC atoms
-#     xyz = np.full((len(res), 14, 3), np.nan, dtype=np.float32)
-#     for l in lines:
-#         if l[:4] != "ATOM":
-#             continue
-#         chain, resNo, atom, aa = l[21:22], int(l[22:26]), ' '+l[12:16].strip().ljust(3), l[17:20]
-#         idx = pdb_idx.index((chain,resNo))
-#         # for i_atm, tgtatm in enumerate(util.aa2long[util.aa2num[aa]]):
-#         for i_atm, tgtatm in enumerate(util.aa2long[util.aa2num[aa]][:14]): # Nate's proposed change
-#             if tgtatm is not None and tgtatm.strip() == atom.strip(): # ignore whitespace
-#                 xyz[idx,i_atm,:] = [float(l[30:38]), float(l[38:46]), float(l[46:54])]
-#                 break
-
-#     # save atom mask
-#     mask = np.logical_not(np.isnan(xyz[...,0]))
-#     xyz[np.isnan(xyz[...,0])] = 0.0
-
-#     # remove duplicated (chain, resi)
-#     new_idx = []
-#     i_unique = []
-#     for i,idx in enumerate(pdb_idx):
-#         if idx not in new_idx:
-#             new_idx.append(idx)
-#             i_unique.append(i)
-
-#     pdb_idx = new_idx
-#     xyz = xyz[i_unique]
-#     mask = mask[i_unique]
-
-#     seq = np.array(seq)[i_unique]
-
-#     out = {
-#         'xyz':xyz, # cartesian coordinates, [Lx14]
-#         'mask':mask, # mask showing which atoms are present in the PDB file, [Lx14]
-#         'idx':np.array([i[1] for i in pdb_idx]), # residue numbers in the PDB file, [L]
-#         'seq':np.array(seq), # amino acid sequence, [L]
-#         'pdb_idx': pdb_idx,  # list of (chain letter, residue number) in the pdb file, [L]
-#     }
-
-#     # heteroatoms (ligands, etc)
-#     if parse_hetatom:
-#         xyz_het, info_het = [], []
-#         for l in lines:
-#             if l[:6]=='HETATM' and not (ignore_het_h and l[77]=='H'):
-#                 info_het.append(dict(
-#                     idx=int(l[7:11]),
-#                     atom_id=l[12:16],
-#                     atom_type=l[77],
-#                     name=l[16:20]
-#                 ))
-#                 xyz_het.append([float(l[30:38]), float(l[38:46]), float(l[46:54])])
-
-#         out['xyz_het'] = np.array(xyz_het)
-#         out['info_het'] = info_het
-
-#     return out
-
-
-# def parse_pdb_lines(lines, parse_hetatom=False, ignore_het_h=True):
-#     # indices of residues observed in the structure
-
-#     res = [(l[22:26],l[17:20]) for l in lines if l[:4]=="ATOM" and l[12:16].strip() in ["CA","P"]]
-#     seq = [util.aa2num[r[1]] if r[1] in util.aa2num.keys() else 20 for r in res]
-#     pdb_idx = [( l[21:22].strip(), int(l[22:26].strip()) ) for l in lines if l[:4]=="ATOM" and l[12:16].strip() in ["CA","P"]]  # chain letter, res num
-
-#     # 4 BB + up to 10 SC atoms
-#     xyz = np.full((len(res), 14, 3), np.nan, dtype=np.float32)
-#     for l in lines:
-#         if l[:4] != "ATOM":
-#             continue
-#         chain, resNo, atom, aa = l[21:22], int(l[22:26]), ' '+l[12:16].strip().ljust(3), l[17:20]
-#         idx = pdb_idx.index((chain,resNo))
-#         # for i_atm, tgtatm in enumerate(util.aa2long[util.aa2num[aa]]):
-#         for i_atm, tgtatm in enumerate(util.aa2long[util.aa2num[aa]][:14]): # Nate's proposed change
-#             if tgtatm is not None and tgtatm.strip() == atom.strip(): # ignore whitespace
-#                 xyz[idx,i_atm,:] = [float(l[30:38]), float(l[38:46]), float(l[46:54])]
-#                 break
-
-#     # save atom mask
-#     mask = np.logical_not(np.isnan(xyz[...,0]))
-#     xyz[np.isnan(xyz[...,0])] = 0.0
-
-#     # remove duplicated (chain, resi)
-#     new_idx = []
-#     i_unique = []
-#     for i,idx in enumerate(pdb_idx):
-#         if idx not in new_idx:
-#             new_idx.append(idx)
-#             i_unique.append(i)
-
-#     pdb_idx = new_idx
-#     xyz = xyz[i_unique]
-#     mask = mask[i_unique]
-
-#     seq = np.array(seq)[i_unique]
-
-#     out = {
-#         'xyz':xyz, # cartesian coordinates, [Lx14]
-#         'mask':mask, # mask showing which atoms are present in the PDB file, [Lx14]
-#         'idx':np.array([i[1] for i in pdb_idx]), # residue numbers in the PDB file, [L]
-#         'seq':np.array(seq), # amino acid sequence, [L]
-#         'pdb_idx': pdb_idx,  # list of (chain letter, residue number) in the pdb file, [L]
-#     }
-
-#     # heteroatoms (ligands, etc)
-#     if parse_hetatom:
-#         xyz_het, info_het = [], []
-#         for l in lines:
-#             if l[:6]=='HETATM' and not (ignore_het_h and l[77]=='H'):
-#                 info_het.append(dict(
-#                     idx=int(l[7:11]),
-#                     atom_id=l[12:16],
-#                     atom_type=l[77],
-#                     name=l[16:20]
-#                 ))
-#                 xyz_het.append([float(l[30:38]), float(l[38:46]), float(l[46:54])])
-
-#         out['xyz_het'] = np.array(xyz_het)
-#         out['info_het'] = info_het
-
-#     return out
-
-# def parse_pdb_lines(lines, parse_hetatom=False, ignore_het_h=True):
-#     # indices of residues observed in the structure
-#     # res = [(l[22:26],l[17:20]) for l in lines if l[:4]=="ATOM" and l[12:16].strip()=="CA"]
-#     # res = [(l[21:22].strip(), l[22:26],l[17:20],l[60:66].strip()) for l in lines if l[:4]=="ATOM" and l[12:16].strip() in ["CA","P"]] # (chain letter, res num, aa, bfactor / plddt)
-#     res = [(l[21:22].strip(), l[22:26],l[17:20],l[60:66].strip()) for l in lines if l[:4]=="ATOM" and l[12:16].strip() in ["CA","C5'"]] # (chain letter, res num, aa, bfactor / plddt)
-
-#     seq = [util.aa2num[r[1]] if r[1] in util.aa2num.keys() else 20 for r in res]
-#     # pdb_idx = [( l[21:22].strip(), int(l[22:26].strip()) ) for l in lines if l[:4]=="ATOM" and l[12:16].strip()=="CA"]  # chain letter, res num
-#     # pdb_idx = [( l[21:22].strip(), int(l[22:26].strip()) ) for l in lines if l[:4]=="ATOM" and l[12:16].strip() in ["CA","P"]]  # chain letter, res num
-#     pdb_idx = [( l[21:22].strip(), int(l[22:26].strip()) ) for l in lines if l[:4]=="ATOM" and l[12:16].strip() in ["CA","C5'"]]  # chain letter, res num
-
-#     # 4 BB + up to 10 SC atoms
-#     xyz = np.full((len(res), 14, 3), np.nan, dtype=np.float32)
-#     for l in lines:
-#         if l[:4] != "ATOM":
-#             continue
-#         chain, resNo, atom, aa = l[21:22], int(l[22:26]), ' '+l[12:16].strip().ljust(3), l[17:20]
-#         # try:
-#         idx = pdb_idx.index((chain,resNo))
-#         for i_atm, tgtatm in enumerate(util.aa2long[util.aa2num[aa]][:14]): # Nate's proposed change
-#         # for i_atm, tgtatm in enumerate(util.aa2long[util.aa2num[aa]]):
-#             if tgtatm is not None and tgtatm.strip() == atom.strip(): # ignore whitespace
-#                 xyz[idx,i_atm,:] = [float(l[30:38]), float(l[38:46]), float(l[46:54])]
-#                 break
-
-
-#     # save atom mask
-#     mask = np.logical_not(np.isnan(xyz[...,0]))
-#     xyz[np.isnan(xyz[...,0])] = 0.0
-
-#     # remove duplicated (chain, resi)
-#     new_idx = []
-#     i_unique = []
-#     for i,idx in enumerate(pdb_idx):
-#         if idx not in new_idx:
-#             new_idx.append(idx)
-#             i_unique.append(i)
-
-#     pdb_idx = new_idx
-#     xyz = xyz[i_unique]
-#     mask = mask[i_unique]
-
-#     seq = np.array(seq)[i_unique]
-
-#     out = {
-#         'xyz':xyz, # cartesian coordinates, [Lx14]
-#         'mask':mask, # mask showing which atoms are present in the PDB file, [Lx14]
-#         'idx':np.array([i[1] for i in pdb_idx]), # residue numbers in the PDB file, [L]
-#         'seq':np.array(seq), # amino acid sequence, [L]
-#         'pdb_idx': pdb_idx,  # list of (chain letter, residue number) in the pdb file, [L]
-#     }
-
-#     # heteroatoms (ligands, etc)
-#     if parse_hetatom:
-#         xyz_het, info_het = [], []
-#         for l in lines:
-#             if l[:6]=='HETATM' and not (ignore_het_h and l[77]=='H'):
-#                 info_het.append(dict(
-#                     idx=int(l[7:11]),
-#                     atom_id=l[12:16],
-#                     atom_type=l[77],
-#                     name=l[16:20]
-#                 ))
-#                 xyz_het.append([float(l[30:38]), float(l[38:46]), float(l[46:54])])
-
-#         out['xyz_het'] = np.array(xyz_het)
-#         out['info_het'] = info_het
-#     return out
-
 
 def parse_a3m(filename):
 
